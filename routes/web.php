526b2879bd14b008ceb25f514853640c7dd61ec8
User <partht.cirklestudio@gmail.com>
2021-09-22 12:41:38 +0530

    second commit of osiset laravel

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware(['verify.shopify'])->name('home');
+
+Route::get('user', [UserController::class, 'index']);
